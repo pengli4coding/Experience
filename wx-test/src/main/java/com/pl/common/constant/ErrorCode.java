ee3a13d34f37a6fc851d3d3f2fc1d3285362b5c8
pengli4coding <pengli4coding@163.com>
2018-11-05 19:19:21 +0800

    modify

--- wx-test/src/main/java/com/pl/common/constant/ErrorCode.java
+++
@@ -1,8 +0,0 @@
-package com.pl.common.constant;
-
-public interface ErrorCode {
-
-	static final Integer SUCCESS_CODE = 200;
-
-	static final Integer WX_RESPONSE_ERROR_CODE = 40001;//微信平台响应错误
-}
--- wx-test/src/main/java/com/pl/vo/TemplateMessageVo.java
+++
@@ -1,5 +0,0 @@
-package com.pl.vo;
-
-public class TemplateMessageVo extends BaseVo{
-	
-}
